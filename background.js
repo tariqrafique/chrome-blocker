424b5185616f00e4c2bbf0c4d4447bad0f29eb43
Tariq Rafique <mrafique@salesforce.com>
2019-03-19 19:17:59 -0700

    first

---
+++ background.js
@@ -0,0 +1,17 @@
+console.log(">>> Hey");
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+      return {cancel: true};
+    },
+    {urls: [ '*://*.twitter.com/*',
+             '*://*.facebook.com/*',
+             '*://*.theverge.com/*',
+             '*://*.thestar.com/*',
+             '*://*.theglobeandmail.com/*',
+             '*://techcrunch.com/*',
+             '*://*.cbc.ca/*',
+             '*://cbcnews.ca/*',
+             '*://*.dawn.com/*',
+        ]},
+    ["blocking"]);
