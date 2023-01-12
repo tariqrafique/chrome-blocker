4a5ed28bcc1c1a583f2af067543b238e3d68d5fe
Tariq Rafique <mrafique@salesforce.com>
2023-01-12 10:34:14 -0800

    wip

--- background.js
+++ background.js
@@ -5,27 +5,31 @@ chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
       return cancelobj;
     },
-    {urls: [ '*://*.twitter.com/*',
-             '*://*.facebook.com/*',
-             '*://*.theverge.com/*',
-             '*://*.thestar.com/*',
-             '*://*.theglobeandmail.com/*',
-             '*://techcrunch.com/*',
-             '*://*.cbc.ca/*',
-             '*://cbcnews.ca/*',
-             '*://*.dawn.com/*',
-             '*://news.google.com/*',
-             '*://*.cnn.com/*',
-             '*://*.lifehacker.com/*',
-             '*://*.engadget.com/*',
-             '*://*.reddit.com/*',
-             '*://*.gizmodo.com/*',
-             '*://*.nytimes.com/*',
-             '*://*.bing.com/news/*',
-             '*://*.seattletimes.com/*',
-             '*://aloha--c.documentforce.com/*',
-             '*://*.bbc.com/*',
-             '*://*.bbc.co.uk/*',
-             '*://*.bbcnews.com/*'
-        ]},
-    ["blocking"]);
+    {
+      urls: [ 
+        '*://*.twitter.com/*',
+        // '*://*.facebook.com/*',
+        '*://*.theverge.com/*',
+        '*://*.thestar.com/*',
+        '*://*.theglobeandmail.com/*',
+        '*://techcrunch.com/*',
+        '*://*.cbc.ca/*',
+        '*://cbcnews.ca/*',
+        '*://*.dawn.com/*',
+        '*://news.google.com/*',
+        '*://*.cnn.com/*',
+        '*://*.lifehacker.com/*',
+        '*://*.engadget.com/*',
+        '*://*.reddit.com/*',
+        '*://*.gizmodo.com/*',
+        '*://*.nytimes.com/*',
+        '*://*.bing.com/news/*',
+        '*://*.seattletimes.com/*',
+        '*://*.washingtonpost.com/*',
+        // '*://aloha--c.documentforce.com/*', // aloha banner
+        '*://*.bbc.com/*',
+        '*://*.bbc.co.uk/*',
+        '*://*.bbcnews.com/*'
+    ]
+  },
+  ["blocking"]);
